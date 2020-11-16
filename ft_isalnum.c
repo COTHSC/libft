99eb886caf1010a068f0ba6b9ddaa45ea65157d8
JCTS <thierryscully.colomban@gmail.com>
2020-11-16 22:02:27 +0100

    ft_is first try

---
+++ ft_isalnum.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 21:43:32 by jescully          #+#    #+#             */
+/*   Updated: 2020/11/16 21:55:13 by jescully         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	       return (1);
+	else
+		return (0);
+}
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_isalnum(int c)
+{
+	if(ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	else
+		return (0);
+}
