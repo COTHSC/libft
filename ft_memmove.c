10a48d5c55b1a82d31da355d2ca145a583dd2d51
jean <thierryscully.colomban@gmail.com>
2020-12-02 11:38:38 +0100

    tweaks

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:24:13 by jescully          #+#    #+#             */
-/*   Updated: 2020/12/02 11:38:07 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/02 11:38:35 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	if ((char *)dest < (char *)src)
 		ft_memcpy(dest, src, n);
 	else
-		while (cn-- > 0)
+		while (n-- > 0)
 			((char *)dest)[n] = ((char *)src)[n];
 	return (dest);
 }
