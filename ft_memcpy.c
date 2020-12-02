406361efe335f45c415e195169288abc45b134b2
jean <thierryscully.colomban@gmail.com>
2020-12-02 11:37:04 +0100

    tweaks

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 14:46:51 by jescully          #+#    #+#             */
-/*   Updated: 2020/12/02 10:39:34 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/02 11:37:02 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	while (i < cn)
+	while (i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
