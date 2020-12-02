0310146a80f113680efc915b8a00629cf4a11509
jean <thierryscully.colomban@gmail.com>
2020-12-02 10:33:52 +0100

    tweaks

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 14:46:51 by jescully          #+#    #+#             */
-/*   Updated: 2020/12/01 16:43:10 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/02 10:33:45 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,13 @@
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	size_t			i;
-	unsigned char	*cdest;
-	unsigned char	*csrc;
-	int				cn;
 
-	cn = (int)n;
-	cdest = dest;
-	csrc = (unsigned char*)src;
 	i = 0;
 	while (i < n)
 	{
-		cdest[i] = csrc[i];
-		if (cdest[i] == (unsigned char)c)
-			return (&cdest[++i]);
+		((char *)dest)[i] = ((char *)src)[i];
+		if (dest[i] == (unsigned char)c)
+			return (&dest[++i]);
 		i++;
 	}
 	return (NULL);
