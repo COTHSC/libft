1520a0e6a217f4fab53c2d8c90a01f23bfe9e6e5
jean <thierryscully.colomban@gmail.com>
2020-12-02 12:00:34 +0100

    tweaks

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 12:14:22 by jescully          #+#    #+#             */
-/*   Updated: 2020/12/02 11:59:17 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/02 12:00:31 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	size_t				i;
 	unsigned const char *str1;
 	unsigned const char *str2;
 
