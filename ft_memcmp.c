a5b4a2d7bc104c41b9cced42e069dd275f307cf0
jean <thierryscully.colomban@gmail.com>
2020-12-02 10:45:31 +0100

    tweaks

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 12:14:22 by jescully          #+#    #+#             */
-/*   Updated: 2020/11/25 09:59:56 by jescully         ###   ########.fr       */
+/*   Updated: 2020/12/02 10:45:29 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,15 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	int				i;
-	unsigned char	*cs1;
-	unsigned char	*cs2;
-	int				cn;
+	size_t	i;
 
-	cn = (int)n;
 	i = 0;
-	cs1 = (unsigned char *)s1;
-	cs2 = (unsigned char *)s2;
-	while (i < cn)
+	while (i < n)
 	{
-		if (cs1[i] == cs2[i])
+		if (s1[i] == s2[i])
 			i++;
 		else
-			return (cs1[i] - cs2[i]);
+			return (s1[i] - s2[i]);
 	}
 	return (0);
 }
