3720ade2d027019e6488a3737de80b93e233793f
jean <thierryscully.colomban@gmail.com>
2020-12-02 10:56:17 +0100

    tweaks

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 13:43:14 by jescully          #+#    #+#             */
-/*   Updated: 2020/12/01 21:04:00 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/02 10:56:12 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **alst, t_list *new);
 int				ft_lstsize(t_list *lst);
 void			*ft_memset(char *s, int c, int n);
-void			ft_bzero(char *s, size_t n);
+void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
